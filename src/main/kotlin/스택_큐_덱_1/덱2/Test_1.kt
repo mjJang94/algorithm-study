ded1a6b08849fe058e064df3331f4a4340fdf441
mjJang94 <alswhddl10@naver.com>
2025-07-21 09:28:03 +0900

    (스택 큐 덱 1) 덱2

---
+++ src/main/kotlin/스택_큐_덱_1/덱2/Test_1.kt
@@ -0,0 +1,22 @@
+package 스택_큐_덱_1.덱2
+
+fun main() {
+    val n = readln().toInt()
+    val sb = StringBuilder()
+    val deque = ArrayDeque<Int>()
+
+    repeat(n) {
+        val order = readln().split(" ")
+        when(order.first()) {
+            "1" -> deque.addFirst(order.last().toInt())
+            "2" -> deque.addLast(order.last().toInt())
+            "3" -> deque.removeFirstOrNull()?.let { sb.appendLine(it) } ?: sb.appendLine("-1")
+            "4" -> deque.removeLastOrNull()?.let { sb.appendLine(it) } ?: sb.appendLine("-1")
+            "5" -> sb.appendLine(deque.size)
+            "6" -> if (deque.isEmpty()) sb.appendLine("1") else sb.appendLine("0")
+            "7" -> deque.firstOrNull()?.let { sb.appendLine(it) } ?: sb.appendLine("-1")
+            "8" -> deque.lastOrNull()?.let { sb.appendLine(it) } ?: sb.appendLine("-1")
+        }
+    }
+    print(sb)
+}
