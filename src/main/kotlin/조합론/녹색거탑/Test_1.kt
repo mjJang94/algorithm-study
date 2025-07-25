a17b84fac5439db8bcab7a0faf7e3c6c29997d11
mjJang94 <alswhddl10@naver.com>
2025-07-25 13:52:25 +0900

    (조합론) 녹색거탑

---
+++ src/main/kotlin/조합론/녹색거탑/Test_1.kt
@@ -0,0 +1,11 @@
+package 조합론.녹색거탑
+
+fun main() {
+    val n = readln().toInt()
+    print(dfs(0, 0, n))
+}
+
+fun dfs(level: Int, index: Int, n: Int): Int {
+    if (level == n) return 1
+    return dfs(level + 1, index, n) + dfs(level + 1, index + 1, n)
+}
