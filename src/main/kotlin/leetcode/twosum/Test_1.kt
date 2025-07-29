046caea6135809f8a19ebb39e63fa66e0c7abe59
mjJang94 <alswhddl10@naver.com>
2025-07-29 15:53:11 +0900

    Two Sum

---
+++ src/main/kotlin/leetcode/twosum/Test_1.kt
@@ -0,0 +1,19 @@
+package leetcode.twosum
+
+fun main() {
+    val s = twoSum(intArrayOf(3, 2, 4), target = 6)
+    print(s.toString())
+}
+
+fun twoSum(nums: IntArray, target: Int): IntArray {
+    val sb = StringBuilder()
+    for (i in 0 until nums.size) {
+        for (j in i until nums.size) {
+            if (i == j) continue
+            if (nums[i] + nums[j] == target) {
+                sb.append(i).append(" ").append(j)
+            }
+        }
+    }
+    return sb.split(" ").map { it.toInt() }.toIntArray()
+}
